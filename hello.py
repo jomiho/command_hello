4579c2eee04b690b71b4516c8f3fa1ce55b2a2c4
jominho <a2390446@naver.comgit config --global user.email a2390446@naver.com>
2018-09-19 10:33:01 +0900

    creat "hello world" program

---
+++ hello.py
@@ -0,0 +1,44 @@
+printf()[D"hello world")
+
+
+
+
+
+git status
+cat >hello.py
+
+
+
+[A[A[[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3[C[C[C[C[C[C[C[[A[C[3~[C[C[C[C[C[C[C[C[C[C[A[A[A[A[A[A[A[A[C[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+
+[B[B
+
+
+
+
+[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[B[B[B[B[Bclear
+
+cat hello.py
+python hello.py
+
+
+
+
+
+
+[A[A[A[A[A[A[A[A[A[[[[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C[C[C[C[C[C[C[A[A[A[C[A[[Cewf
+ef
+
+
+
+
+
+
+
+
+
+
+
+
+\
+\
